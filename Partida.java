011913213d45c52dd0bb0636afe773830b6de001
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-04-05 18:21:45 +0200

    Implementacio de classes a partida i encapsulacio

--- Partida.java
+++ Partida.java
@@ -1,4 +1,19 @@
 package ProjecteConnecta4;
 
 public class Partida {
+
+    private Jugador jugadorUn;
+    private Jugador jugadorDos;
+    private Casella casella;
+    private Tauler tauler;
+    private Fitxa fitxa;
+
+
+    public Partida (Jugador jugadorUn, Jugador jugadorDos, Tauler tauler) {
+
+        this.jugadorUn = jugadorUn;
+        this.jugadorDos = jugadorDos;
+        this.tauler = tauler;
+    }
+
 }
