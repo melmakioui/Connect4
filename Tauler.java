6ea037e5dda3b3644f948c78ba218219daf0f98e
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-04-05 18:01:53 +0200

    Agregacio d'atributs i constructor Tauler

--- Tauler.java
+++ Tauler.java
@@ -1,5 +1,23 @@
 package ProjecteConnecta4;
 
 public class Tauler {
-    
+
+    private char[][] tauler;
+    private Casella casella;
+    private static final int CONNECTA = 4;
+
+    public Tauler(){
+        this.tauler = new char[7][8];
+        this.casella = new Casella();
+
+    }
+
+    private void initTablero() {
+        for (int i = 0; i < tauler.length; i++) {
+            for (int j = 0; j < tauler[0].length; j++) {
+                tauler[i][j] = casella.estaDesocupada();
+            }
+        }
+    }
+
 }
