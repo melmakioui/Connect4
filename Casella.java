94a213b371140354095a729cc1b191a042a32573
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-04-05 17:44:56 +0200

    Creacio classe Casella

---
+++ Casella.java
@@ -0,0 +1,22 @@
+package ProjecteConnecta4;
+
+public class Casella {
+
+    private char desocupada = '*';
+
+
+    public Casella(){
+
+
+    }
+
+
+    public void estaOcupada(char fitxa){
+
+    }
+
+    public char estaDesocupada(){
+        return this.desocupada;
+    }
+
+}
