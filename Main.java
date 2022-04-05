065fa229964fba129e2e1763950e2222988d546c
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-04-05 18:49:32 +0200

    Instancies Classes per la iniciar la partida

--- Main.java
+++ Main.java
@@ -1,4 +1,21 @@
 package ProjecteConnecta4;
 
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        Fitxa fitxaJugadorUn = new Fitxa('X');
+        Jugador jugadorUn = new Jugador("Player 1", fitxaJugadorUn);
+
+        Fitxa fitxaJugadorDos = new Fitxa('O');
+        Jugador jugadorDos = new Jugador("Player 2", fitxaJugadorDos);
+
+        Tauler tauler = new Tauler();
+
+    }
+
+
+
+
 }
