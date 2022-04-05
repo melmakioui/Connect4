8d2b96ce2c1f13b939a7904edd3ea83715277e3f
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-04-05 18:11:49 +0200

    Implementacio d'entrada Scanner  i comprovacio
    Entrada per triar on tirar la fitxa .

--- entradasortida/EntradaSortida.java
+++ entradasortida/EntradaSortida.java
@@ -1,6 +1,27 @@
 package ProjecteConnecta4.entradasortida;
 
+import ProjecteConnecta4.Jugador;
+
+import java.util.Scanner;
+
 public class EntradaSortida {
 
+
+    private static final int MAXIM_FILES = 7;
+
+    public static int triaPosicio(Jugador jugador) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("TIRA " + jugador.getNom() );
+        int posicio = entrada.nextInt();
+
+        while (posicio > MAXIM_FILES || posicio < 0) {
+            System.out.println("TRIA UNA POICIO CORRECTAMENT...");
+            posicio = entrada.nextInt();
+        }
+
+        return posicio;
+    }
 
 }
