72ebd1995aab01679272d6e22fefe61bc367350f
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-04-05 17:48:58 +0200

    Creacio classe Fitxa

---
+++ Fitxa.java
@@ -0,0 +1,25 @@
+package ProjecteConnecta4;
+
+public class Fitxa {
+
+    private int fitxes = 25;
+    private char fitxa;
+
+
+    public Fitxa (char fitxa) {
+        this.fitxa = fitxa;
+    }
+
+
+    public int getFitxes() {
+        return fitxes;
+    }
+
+    public void restaFitxa(){
+        this.fitxes -= 1;
+    }
+
+    public char getCaraFitxa() {
+        return fitxa;
+    }
+}
